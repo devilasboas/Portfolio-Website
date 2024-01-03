577c7d714204f6d5abf405e18b1603133c143e63
camila de vilas boas <camiladevilasboas@gmail.com>
2024-01-02 21:55:54 -0300

    modified file html

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,11 @@ if(navClose){
     })
 }
 
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
