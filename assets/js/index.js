fc5ea4fd3289b88ed62f254a83eaf7f5b9047900
camila de vilas boas <camiladevilasboas@gmail.com>
2024-01-14 17:22:53 -0300

    modified file js

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,15 +22,16 @@ modalClose.forEach((modalClose) => {
 })
 /*=============== SWIPER TESTIMONIAL ===============*/
 const swiperTestimonial = new Swiper('.testimonial__swiper', {
-    loop: true, 
-    spaceBetween:32,
-    grabCursor: true,
+    loop: true,
+    spaceBetween: 32,
+    grabCursor:true,
 
     pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets:true,
-        clickable: true,
-    },
-})
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+      clickable: true,
+    },  
+});
+  
 
 /*=============== SHOW SCROLL UP ===============*/ 
